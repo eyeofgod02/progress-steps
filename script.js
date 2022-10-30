d1f2b9056baa51a9a173b8dd4c0f129a5945ead9
eyeofgod02 <95542469+eyeofgod02@users.noreply.github.com>
2022-10-30 15:43:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ console.log(progressBar)
 
 nextBtn.addEventListener('click', () => {
     if(step > 3) return;
+    if(step === 3) nextBtn.classList.add('disabled')
+    backBtn.classList.remove('disabled')
     step += 1;
     progressBar.style.width = step * 80 + 'px';
     circles[step].classList.add('active')
@@ -16,6 +18,8 @@ nextBtn.addEventListener('click', () => {
 
 backBtn.addEventListener('click', () => {
     if(step < 1) return;
+    if(step === 1) backBtn.classList.add('disabled')
+    nextBtn.classList.remove('disabled')
     step -= 1;
     progressBar.style.width = step * 80 + 'px';
     circles[step + 1].classList.remove('active')
